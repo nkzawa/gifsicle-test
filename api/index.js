f1fa5f6667ccc9ef15c23e66b26ebcf41dfa9068
nkzawa <naoyuki.kanezawa@gmail.com>
2020-12-17 23:47:54 +0900

    initial commit

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const { spawn } = require('child_process');
+const fs = require('fs');
+const gifsicle = require('gifsicle');
+const { join } = require('path');
+
+module.exports = (req, res) => {
+  // fake to resolve the file
+  fs.createReadStream(join(__dirname, '../public/input.gif'));
+
+  const child = spawn(gifsicle, ['--colors', '256', join(__dirname, '../public/input.gif')]);
+  child.stdout.on('data', (data) => {
+    res.write(data);
+  })
+  child.stderr.on('data', (data) => {
+    console.log(data.toString());
+  })
+  child.on('close', (code) => {
+    console.log('Image minified: ', code);
+    res.end();
+  });
+
+  res.setHeader('content-type', 'image/gif');
+}
